7b42dfc3ac691d4a695ce9a5d0e51fa364cb5ea9
“Ron <wheresronald0@icloud.com>
2021-09-30 15:12:06 -0400

    added startGame container and layed out contents with some styling

---
+++ rn-guess-a-number/container/startGameScreen.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { View, Text, StyleSheet, Button, TextInput } from "react-native";
+
+const StartGameScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>Start a New Game</Text>
+      <View style={styles.InputsContainer}>
+        <Text>Select a Number</Text>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <Button title="Reset" onPress={() => {}} />
+          <Button title="I'm Sure" onPress={() => {}} />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 25,
+  },
+  InputsContainer: {
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center",
+  },
+});
+
+export default StartGameScreen;
